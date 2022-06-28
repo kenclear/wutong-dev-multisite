b31d5d237dc698a2dbd69b0bf6ca2ef2138863c1
Jeff Cleverley <jeff@jeffcleverley.com>
2022-06-28 18:02:31 +0800

    all general gp plugin integrations

---
+++ wp-content/mu-plugins/mu-gridpane-disable-xmlrpc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * GridPane Disable XMLRPC
+ *
+ * @package         GridPane\GridPaneDisableXmlrpc
+ * @since           0.1.0
+ * @author          Jeff Cleverley
+ * @license         GNU-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:     GridPane Disable XMLRPC
+ * Plugin URI:      https://gridpane.com
+ * Description:     Disable XMLRPC
+ * Version:         0.1.0
+ * Author:          Jeff Cleverley
+ * Author URI:      https://github.com/GridPane-Dev
+ * Author URI:      https://github.com/GridPane-Dev
+ * License:         GPL-2.0+
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+require __DIR__ . '/gridpane-disable-xmlrpc/gridpane-disable-xmlrpc.php';
