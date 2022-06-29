ea5f8bc4b71a0668bd4612867543b53ce6be2e75
gridpane <36479264+gridpane@users.noreply.github.com>
2022-06-29 17:36:12 +0800

    Update predeploy.sh

--- .gpconfig/predeploy.sh
+++ .gpconfig/predeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Site Specific Pre Deploy Script
-# - Runs prior to deployed files being symlinked
-# - Runs from the release/.gpconfig directory
-# - Runs as the site system user
+# Site level access
+# - Runs before files have been deployed 
+# - Runs as site's system user
+# - Runs from within site /.gpconfig dir
 
